f69b6eacd393b7d834e21634c000ec8a6ec68ee7
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-02-22 20:36:01 -0500

    Finished Registration

--- app/src/main/java/com/example/nikola/waterreport/model/Worker.java
+++ app/src/main/java/com/example/nikola/waterreport/model/Worker.java
@@ -4,5 +4,10 @@ package com.example.nikola.waterreport.model;
  * @author Nikola Istvnaic
  */
 
-public class Worker extends User{
+public class Worker extends User {
+    public Worker() {}
+
+    public Worker(String username, String password, String ID) {
+        super(username, password, ID);
+    }
 }
