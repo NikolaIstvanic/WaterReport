9d361549ffc4175f1b46c91be182f05eb9b66a75
Vishvak <vmurahari3@gatech.edu>
2017-02-22 22:07:39 -0500

    added few javadocs

--- app/src/main/java/com/example/nikola/waterreport/controllers/LoginManager.java
+++ app/src/main/java/com/example/nikola/waterreport/controllers/LoginManager.java
@@ -8,9 +8,19 @@ import java.util.Map;
 /**
  * @author Nikola Istvnaic
  */
+
 public class LoginManager {
+    /**
+     * variable for storing mapping betweeen Username and User
+     */
     public static Map<String, User> mappings = new HashMap<>();
 
+    /**
+     *
+     * @param username the username to lookup
+     * @param password the password entered by user
+     * @return true if the mapping exists false elsewhere
+     */
     public static boolean lookup(String username, String password) {
         return !(mappings.get(username) == null || mappings.isEmpty()) && mappings.get(username).authenticate(password);
     }
