56db9f288fed4317ac99b9150eb479ff2892a7aa
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-03-01 00:17:10 -0500

    Updated Report UI

--- app/src/main/java/com/example/nikola/waterreport/controllers/ReportActivity.java
+++ app/src/main/java/com/example/nikola/waterreport/controllers/ReportActivity.java
@@ -1,33 +1,85 @@
 package com.example.nikola.waterreport.controllers;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nikola.waterreport.R;
 import com.example.nikola.waterreport.model.Singleton;
+import com.example.nikola.waterreport.model.WaterReport;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class ReportActivity extends AppCompatActivity {
+    private Spinner source;
+    private Spinner condition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        source = (Spinner) findViewById(R.id.spinner);
+        condition = (Spinner) findViewById(R.id.condition_spinner);
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
         ((TextView) findViewById(R.id.text_time)).setText("Date: " + sdf.format(new Date()));
         ((TextView) findViewById(R.id.user_name)).setText("Username: " + getIntent().getExtras().getString(Intent.EXTRA_USER));
         ((TextView) findViewById(R.id.report_number)).setText("Report ID: " + ++Singleton.id_num);
-        Button submitReport = (Button) findViewById(R.id.submit_report);
+        EditText locationView = (EditText) findViewById(R.id.location);
+        locationView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    submitReport();
+                    return true;
+                }
+                return false;
+            }
+        });
+        final Button submitReport = (Button) findViewById(R.id.submit_report);
         submitReport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                submitReport();
             }
         });
+        /* setup spinner values */
+        Resources res = getResources();
+        List<String> legalLocations = Arrays.asList(res.getStringArray(R.array.type_spinner_values));
+        ArrayAdapter<String> adap = new ArrayAdapter(this,android.R.layout.simple_spinner_item, legalLocations);
+        adap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        source.setAdapter(adap);
+
+        List<String> legalTypes = Arrays.asList(res.getStringArray(R.array.type_spinner_values));
+        ArrayAdapter<String> a = new ArrayAdapter(this,android.R.layout.simple_spinner_item, legalTypes);
+        a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        condition.setAdapter(a);
+    }
+
+    private void submitReport() {
+        Singleton.pseudoDB.add(new WaterReport(String.valueOf(((TextView) findViewById(R.id.user_name)).getText()),
+                String.valueOf(((TextView) findViewById(R.id.text_time)).getText()),
+                Integer.parseInt(String.valueOf(((TextView) findViewById(R.id.report_number)).getText())),
+                String.valueOf(((TextView) findViewById(R.id.location)).getText()),
+                (String) source.getSelectedItem(), (String) condition.getSelectedItem()));
+        Context context = getApplicationContext();
+        CharSequence text = "Report Submitted !!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        finish();
+        toast.show();
     }
 }
--- app/src/main/java/com/example/nikola/waterreport/model/Singleton.java
+++ app/src/main/java/com/example/nikola/waterreport/model/Singleton.java
@@ -1,27 +1,30 @@
 package com.example.nikola.waterreport.model;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Nikola Istvanic
  */
-
 public class Singleton {
     /**
      * variable for storing mapping betweeen Username and User
      */
     public static Map<String, User> mappings = new HashMap<>();
-    public static ArrayList<WaterReport> pseudoDB = new ArrayList<WaterReport>();
+    /**
+     * Database of submitted WaterReports
+     */
+    public static List<WaterReport> pseudoDB = new ArrayList<>();
     /**
      * variable for storing how many water reports have been entered
      */
     public static int id_num = 0;
 
     /**
-     *
+     * Looks if the username to password pairing matches what the User object in the map's password
+     * field is a match.
      * @param username the username to lookup
      * @param password the password entered by user
      * @return true if the mapping exists false elsewhere
@@ -29,6 +32,4 @@ public class Singleton {
     public static boolean lookup(String username, String password) {
         return !(mappings.get(username) == null || mappings.isEmpty()) && mappings.get(username).authenticate(password);
     }
-
-
 }
