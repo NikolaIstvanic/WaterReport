b6f44379cd2f624769ff21b46e103cdec40beb34
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-03-03 00:49:29 -0500

    Adding Google Map

--- app/src/main/java/com/example/nikola/waterreport/controllers/MainActivity.java
+++ app/src/main/java/com/example/nikola/waterreport/controllers/MainActivity.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.nikola.waterreport.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * @author Nikola Istvanic
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
@@ -49,5 +57,25 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         });
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
