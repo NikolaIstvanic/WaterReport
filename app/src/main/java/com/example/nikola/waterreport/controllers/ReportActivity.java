ac354f71981f787c80da97f5f5d1ecb2223edc38
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-03-06 21:46:31 -0500

    Updated WaterReport toString

--- app/src/main/java/com/example/nikola/waterreport/controllers/ReportActivity.java
+++ app/src/main/java/com/example/nikola/waterreport/controllers/ReportActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,17 +26,19 @@ import java.util.List;
 public class ReportActivity extends AppCompatActivity {
     private Spinner source;
     private Spinner condition;
+    private EditText mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        mLocation = (EditText) findViewById(R.id.location);
         source = (Spinner) findViewById(R.id.spinner);
         condition = (Spinner) findViewById(R.id.condition_spinner);
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
         ((TextView) findViewById(R.id.text_time)).setText("Date: " + sdf.format(new Date()));
         ((TextView) findViewById(R.id.user_name)).setText("Username: " + getIntent().getExtras().getString(Intent.EXTRA_USER));
-        ((TextView) findViewById(R.id.report_number)).setText("Report ID: " + ++Singleton.id_num);
+        ((TextView) findViewById(R.id.report_number)).setText("Report ID: " + (Singleton.id_num + 1));
         final Button submitReport = (Button) findViewById(R.id.submit_report);
         submitReport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,16 +60,33 @@ public class ReportActivity extends AppCompatActivity {
     }
 
     private void submitReport() {
-        Singleton.pseudoDB.add(new WaterReport(String.valueOf(((TextView) findViewById(R.id.user_name)).getText()),
-                String.valueOf(((TextView) findViewById(R.id.text_time)).getText()),
-                Integer.parseInt(String.valueOf(((TextView) findViewById(R.id.report_number)).getText()).split(": ")[1]),
-                String.valueOf(((TextView) findViewById(R.id.location)).getText()),
-                (String) source.getSelectedItem(), (String) condition.getSelectedItem()));
-        Context context = getApplicationContext();
-        CharSequence text = "Report Submitted !!";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        finish();
-        toast.show();
+        // Reset errors.
+        mLocation.setError(null);
+        // Store values at the time of the login attempt.
+        String location = mLocation.getText().toString();
+        boolean cancel = false;
+        // Check for a valid location, if the user entered one.
+        if (TextUtils.isEmpty(location)) {
+            mLocation.setError(getString(R.string.error_empty_location));
+            cancel = true;
+        }
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            mLocation.requestFocus();
+        } else {
+            // log water report.
+            Singleton.id_num++;
+            Singleton.pseudoDB.add(new WaterReport(String.valueOf(((TextView) findViewById(R.id.user_name)).getText()),
+                    String.valueOf(((TextView) findViewById(R.id.text_time)).getText()),
+                    Integer.parseInt(String.valueOf(((TextView) findViewById(R.id.report_number)).getText()).split(": ")[1]),
+                    String.valueOf(mLocation.getText()), (String) source.getSelectedItem(), (String) condition.getSelectedItem()));
+            Context context = getApplicationContext();
+            CharSequence text = "Report Submitted !!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            finish();
+            toast.show();
+        }
     }
 }
